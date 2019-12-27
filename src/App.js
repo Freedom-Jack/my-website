8b89c8011815da374be28aa77b23b962be4e69fd
Freedom-Jack <freedomxu6822@gmail.com>
2019-12-26 23:17:19 -0500

    Dependency update

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Components
+import Jumbotron from './components/jumbotron'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+
+      <Jumbotron />Yea
+    </React.Fragment>
   );
 }
 
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const jumbotron = () => {
+    return (
+        <div>Welcome to my page</div>
+    )
+}
+
+export default jumbotron;
