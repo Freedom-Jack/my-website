e44ca6a16451d1b7ce6dc37ee39d20bbd1e6db71
Freedom-Jack <freedomxu6822@gmail.com>
2020-01-04 21:57:18 -0500

    wrap up basic feat

--- src/components/composite_map/index.js
+++ src/components/composite_map/index.js
@@ -3,26 +3,30 @@ import ReactDOM from "react-dom";
 import injectSheet from 'react-jss'
 import { ComposableMap, ZoomableGroup, Geographies, Geography } from "react-simple-maps";
 
-import my_map from "../../maps/countries.json"
+import my_map from "./countries.json"
 
 const styles = theme => ({
     div1: {
-        backgroundColor: "#9fd3ed"
+        backgroundColor: "#9fd3ed",
     }
 })
 
+const map_style = {
+    maxHeight: "520"
+}
 
-const composite = ({ classes }) => (
+
+const composite_map = ({ classes }) => (
     <div className={classes.div1}>
-        <ComposableMap>
-                <Geographies geography={my_map}>
-                    {geographies =>
-                        geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
-                    }
-                </Geographies>
+        <ComposableMap style={map_style}>
+            <Geographies geography={my_map} >
+                {({ geographies }) =>
+                    geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+                }
+            </Geographies>
         </ComposableMap>
     </div>
 
 );
 
-export default injectSheet(styles)(composite);
+export default injectSheet(styles)(composite_map);
