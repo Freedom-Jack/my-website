7092fe0199fb77e407992870587519cd4c7588c6
Freedom-Jack <freedomxu6822@gmail.com>
2025-04-18 16:31:14 -0400

    fix vercel build error

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST='{"pages":{"/_app":[],"/_error":[]},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":true}';
